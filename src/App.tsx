ac156103ec134e216ab56410364e7658fb70c78f
N00bG1rl <marttila.helen@gmail.com>
2022-02-10 10:00:35 +0200

    Data model

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
-import './App.css'
+import Todos from './components/Todos'
+import Todo from './models/todo'
 
 function App() {
-  return <div></div>
+  const todos = [new Todo('Todo2'), new Todo('Todo3')]
+
+  return (
+    <div>
+      <Todos items={todos} />
+    </div>
+  )
 }
 
 export default App
